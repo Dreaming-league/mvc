d0de75ba33ad64faffca7af3b3d0b83b4154c3fe
zhaochunsheng <zhaochunsheng@qq.com>
2016-09-28 14:38:10 +0800

    更新项目为注解模式

--- src/main/java/com/tikie/test/web/HelloWorldController.java
+++ src/main/java/com/tikie/test/web/HelloWorldController.java
@@ -1,19 +1,26 @@
 package com.tikie.test.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-public class HelloWorldController implements Controller {
 
-    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        //1、收集参数、验证参数
-        //2、绑定参数到命令对象
-        //3、将命令对象传入业务对象进行业务处理
-        //4、选择下一个页面
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/test")
+public class HelloWorldController{
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public ModelAndView handleRequest(){
+        /*
+        1、收集参数、验证参数
+        2、绑定参数到命令对象
+        3、将命令对象传入业务对象进行业务处理
+        4、选择下一个页面
+        */
         ModelAndView mv = new ModelAndView();
         //添加模型数据 可以是任意的POJO对象
-        mv.addObject("message", "Hello World! hello !");
+        mv.addObject("message", "Hello World!");
         //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
         mv.setViewName("hello");
         return mv;
