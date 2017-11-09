2ce74f5aaa870b1b031f833fdd57b477b8325c3d
tikie <290315636@qq.com>
2017-11-09 23:41:54 +0800

    更新用户管理接口

--- src/main/java/com/tikie/shiro/controller/UserController.java
+++ src/main/java/com/tikie/shiro/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.tikie.shiro.controller;
 
+import com.tikie.common.util.response.ResponseJson;
+import com.tikie.shiro.entity.User;
 import com.tikie.shiro.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,5 +30,32 @@ public class UserController {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         dateFormat.setLenient(false);
         binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));   //true:允许输入空值，false:不能为空值
+    }
+
+    @RequestMapping(value = "/all",method = RequestMethod.GET)
+    public String getAllUsers(){
+
+        // TODO 做数据权限限制，组里的管理员查看到组下到人员;做好分页
+        return ResponseJson.getInstance().setData(userService.getAll()).toString();
+    }
+
+    @RequestMapping(value = "/getById",method = RequestMethod.POST)
+    public String getById(@RequestParam String id){
+
+        return ResponseJson.getInstance().setData(userService.getById(id)).toString();
+    }
+
+    @RequestMapping(value = "/deleteByIds",method = RequestMethod.DELETE)
+    public String deleteById(@RequestParam String[] ids){
+
+        return ResponseJson.getInstance().setData(userService.deleteByIds(ids)).toString();
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String save(@RequestParam User user){
+        if(StringUtils.isNoneBlank(user.getId())){
+            return ResponseJson.getInstance().setData(userService.add(user)).toString();
+        }
+        return ResponseJson.getInstance().setData(userService.update(user)).toString();
     }
 }
