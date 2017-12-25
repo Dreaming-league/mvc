0f55663f061e1e7b7098aee98debcc13dc0a5451
siandy <1135127431@qq.com>
2017-12-26 00:03:49 +0800

    解决用户管理页面样式问题，可以出版本了

--- src/main/webapp/front/theme/assets/admin/pages/scripts/table-users.js
+++ src/main/webapp/front/theme/assets/admin/pages/scripts/table-users.js
@@ -78,9 +78,9 @@ var TableAdvanced = function () {
             }
         });
 
-        var tableWrapper = $('#sample_1_wrapper'); // datatable creates the table wrapper by adding with id {your_table_jd}_wrapper
+//        var tableWrapper = $('#sample_1_wrapper'); // datatable creates the table wrapper by adding with id {your_table_jd}_wrapper
 
-        tableWrapper.find('.dataTables_length select').select2(); // initialize select2 dropdown
+//        tableWrapper.find('.dataTables_length select').select2(); // initialize select2 dropdown
     }
     
     var testAjax = function(){
